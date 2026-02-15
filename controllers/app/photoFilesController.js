9311c7d183ca9f3b788f86203a0558321ee5ea0a
MrkartikDangi <kpdangi660@gmail.com>
2026-02-15 22:55:39 +0530

    changes in the get photo files api

--- controllers/app/photoFilesController.js
+++ controllers/app/photoFilesController.js
@@ -35,7 +35,6 @@ exports.uploadAttachement = async (req, res) => {
       });
     }
   } catch (error) {
-    console.log('error',error)
     return generic.error(req, res, {
       status: 500,
       message: "Something went wrong !"
@@ -117,12 +116,14 @@ exports.createPhotoFiles = async (req, res) => {
         row.fileName = path.basename(row.path);
         row.folder_name = path.dirname(row.path);
         await PhotoFiles.addPhotoFileData(row)
-        let data = {
-          filePath: `${process.env.Base_Url}${row.folder_name}/${row.fileName}`,
-          fileName: row.fileName,
-          folderPath: `civion/${getScheduleData[0]?.project_name ?? 'DefaultProject'}/photoFiles`
+        if (process.env.DEVTYPE == 'Prod') {
+          let data = {
+            filePath: `${process.env.Base_Url}${row.folder_name}/${row.fileName}`,
+            fileName: row.fileName,
+            folderPath: `civion/${getScheduleData[0]?.project_name ?? 'DefaultProject'}/photoFiles`
+          }
+          await generic.uploadFileToOneDrive(data)
         }
-        await generic.uploadFileToOneDrive(data)
       }
       db.connection.commit()
       return generic.success(req, res, {
