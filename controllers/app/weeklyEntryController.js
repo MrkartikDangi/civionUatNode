a84eaaca9c0b6158d0f1001a5493c03ea9a3a757
MrkartikDangi <kpdangi660@gmail.com>
2026-02-18 14:54:40 +0530

    changes

--- controllers/app/weeklyEntryController.js
+++ controllers/app/weeklyEntryController.js
@@ -87,6 +87,7 @@ exports.createWeeklyEntry = async (req, res) => {
 
     }
   } catch (error) {
+    console.log('error',error)
     db.connection.rollback()
     return generic.error(req, res, {
       status: 500,
