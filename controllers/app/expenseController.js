5cd33c58d161d76a032e5f440b6c6f56658005e7
MrkartikDangi <kpdangi660@gmail.com>
2026-04-07 20:37:07 +0530

    changes

--- controllers/app/expenseController.js
+++ controllers/app/expenseController.js
@@ -66,6 +66,7 @@ exports.addExpense = async (req, res) => {
       }
       await notification.addNotificationData(notificationData)
       let userFcmToken = await generic.selectData('kps_users', { is_boss: '1', fcm_device_id: 'IS NOT NULL' }, ['fcm_device_id'])
+      console.log('userFcmToken',userFcmToken)
       if (userFcmToken.length) {
         for (let row of userFcmToken) {
           let notificationFcmData = {
