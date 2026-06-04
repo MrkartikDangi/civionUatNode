c22f13a606a690db48d77a830ba1fd9b4969e0a6
MrkartikDangi <kpdangi660@gmail.com>
2026-06-05 01:47:14 +0530

    changes

--- models/leaveModel.js
+++ models/leaveModel.js
@@ -42,7 +42,7 @@ Leaves.getLeavesList = (postData) => {
         whereCondition += ` AND kla.leave_manager_id = '${postData.filter.leave_manager_id}'`
     }
     return new Promise((resolve, reject) => {
-    let query = `SELECT kla.id,kla.user_id,kla.leave_approval_level,ku.username AS applied_by,kla.from_date,kla.to_date,kla.reason,klt.leave_name AS leave_type,kla.status,kla.no_of_days,kla.leave_manager_id,
+    let query = `SELECT kla.id,kla.user_id,kla.leave_approval_level,COALESCE(NULLIF(ku.username, ''), CONCAT(COALESCE(ku.firstName, ''), ' ', COALESCE(ku.lastName, ''))) AS applied_by,kla.from_date,kla.to_date,kla.reason,klt.leave_name AS leave_type,kla.status,kla.no_of_days,kla.leave_manager_id,
                                 (
                                     SELECT GROUP_CONCAT(username SEPARATOR ', ')
                                     FROM kps_users
