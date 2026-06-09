613c18604937813d53cd240eace2f2eb1d1b9766
MrkartikDangi <kpdangi660@gmail.com>
2026-06-10 00:36:37 +0530

    changes

--- controllers/app/leaveController.js
+++ controllers/app/leaveController.js
@@ -273,6 +273,7 @@ exports.updateLeaveData = async (req, res) => {
         }
 
     } catch (error) {
+        console.log('error',error)
         db.connection.rollback()
         return generic.error(req, res, {
             status: 500,
